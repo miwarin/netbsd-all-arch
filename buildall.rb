af18be67fa11cf0e90dae82b5f33bf695be4bb89
Susumu Miwa <miwarin@gmail.com>
2014-03-09 00:23:33 +0900

    MACHINE_ARCHをすべてビルドして結果をメールする

--- buildall.rb
+++ buildall.rb
@@ -3,8 +3,8 @@
 #
 # for NetBSD
 #
-# buildall.rb /usr/src/sys/arch/README
-#
+
+require 'mail'
 
 def get_all_arch(arch_readme)
   :ST_DELIM1
@@ -33,10 +33,70 @@ def get_all_arch(arch_readme)
   return archs
 end
 
+def do_build(machine_arch)
+  src_dir = '/usr/src'
+  result_file = "#{src_dir}/#{machine_arch}.build.result"
+  Dir.chdir(src_dir) {|path|
+    cmd = "./build.sh -m #{machine_arch} build >#{result_file} 2>&1"
+    system(cmd)
+  }
+  return result_file
+end
+
+def result_sccess?(result_file)
+  result = File.open(result_file).read()
+  
+  if result.include?('*** BUILD ABORTED ***')
+    return false
+  end
+  return true
+end
+
+def build_message(arch, success)
+  message = ''
+  if success == true
+    message = "#{arch} success"
+  else
+    message = "#{arch} failure"
+  end
+  return message
+end
+
+def send_mail(message)
+  options = {
+    :address              => "smtp.example.jp",
+    :port                 => 25,
+    :domain               => 'example.jp',
+    :user_name            => 'USERNAME',
+    :password             => 'PASSWORD',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+
+  Mail.defaults do
+    delivery_method :smtp, options
+  end
+
+  mail = Mail.new
+  mail.from = 'from@example.jp'
+  mail.to = 'to@example.jp'
+  mail.subject = 'NetBSD build.sh result'
+  mail.charset ='iso-2022-jp'
+  mail.add_content_transfer_encoding
+  mail.body  = message
+  mail.deliver
+end
+
 def main(argv)
-  arch_readme = argv[0]
-  archs = get_all_arch(arch_readme)
-  puts archs
+  arch_readme = '/usr/src/sys/arch/README'
+  machine_arch = get_all_arch(arch_readme)
+  
+  machine_arch.each {|arch|
+    result_file = do_build(arch)
+    ok = result_sccess?(result_file)
+    message = build_message(arch, ok)
+    send_mail(message)
+  }
 end
 
 main(ARGV)
